1af5c3f426fdf60364540c355e66849c4a46c118
abdallahshanaah <ayousef18@uwcchina.org>
2020-10-21 17:09:39 +0300

    files

---
+++ __test__/model-finder.test.js
@@ -0,0 +1,17 @@
+' use strict'
+
+// const { server } = require('../lib/server.js');
+// const supertest = require('supertest');
+// const mockRequest = supertest(server);
+// const getModel = require('../middleware/model-finder.js')
+
+// router.param('model',getModel)
+
+
+// describe('modelFinder', () => {
+//     it('should get model from url', async () => {
+//       await mockRequest.get('/api/v1/test').then((results) => {
+//         expect(results.text).toBe(dateString);
+//       });
+//     });
+//   });
--- __test__/timeStamp.test.js
+++ __test__/timeStamp.test.js
@@ -2,10 +2,19 @@ const { server } = require('../lib/server.js');
 const supertest = require('supertest');
 const mockRequest = supertest(server);
 
+var currentDate = new Date();
+
+var day = currentDate.getDate();
+var month = currentDate.getMonth();
+var year = currentDate.getFullYear();
+
+var dateString = day + "/" + (month + 1) + "/" + year;
+
+
 describe('api server', () => {
   it('should add timeStamp to req', async () => {
     await mockRequest.get('/time').then((results) => {
-      expect(results.text).toBe("19/10/2020");
+      expect(results.text).toBe(dateString);
     });
   });
 });
